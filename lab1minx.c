2454586b84cd46650f6519b3b48be6a5af8f18f4
6704062610356 <s6704062610356@email.kmutnb.ac.th>
2024-11-26 15:00:54 +0700

    ข้อ 2

--- lab1minx.c
+++ lab1minx.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	int FirstNumber = 25;
-	int SecondNumber = 50;
+	int FirstNumber,SecondNumber;
+	printf("Enter First number: ");
+    scanf("%d", &FirstNumber);
+    printf("Enter Second number: ");
+    scanf("%d", &SecondNumber);
 	if(FirstNumber>SecondNumber){
 	printf("First number is greater than Second Number");
 	} else{
